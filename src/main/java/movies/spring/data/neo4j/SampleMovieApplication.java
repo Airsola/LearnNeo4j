926e63f40814120dc7f8634d2b702e7eef61f1f8
Sola <airsolakey@icloud.com>
2017-03-23 15:27:40 +0800

    init project

---
+++ src/main/java/movies/spring/data/neo4j/SampleMovieApplication.java
@@ -0,0 +1,20 @@
+package movies.spring.data.neo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+/**
+ * @author Michael Hunger
+ * @author Mark Angrish
+ * testRun
+ */
+@SpringBootApplication
+@EntityScan("movies.spring.data.neo4j.domain")
+
+public class SampleMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMovieApplication.class, args);
+	}
+}
